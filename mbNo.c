8620887b52257b72c8c28051f23663ff45c0ff5f
Mahesh kumar <mahesh.mahi24102004@gmail.com>
2024-02-03 20:43:50 +0530

    first commit

--- mbNo.c
+++ mbNo.c
@@ -1,5 +1,21 @@
+/*question----> write a program that must accept a list of mobile numbers (in separate lines) as the input.
+The character q or Q represents the end of the input. 
+The program must print the number of invalid mobile numbers among the given list of mobile numbers as the output.
+A valid mobile number contains exactly 10 digits.*/
+
+
 #include <stdio.h>
 #include <string.h>
+#include<ctype.h>
+int isNumeric(const char *str) {
+    while (*str) {
+        if (*str < '0' || *str > '9') {
+            return 0; // Non-numeric character found
+        }
+        str++;
+    }
+    return 1; // All characters are numeric
+}
 
 int main() {
     char mobileNumber[11];
@@ -10,15 +26,14 @@ int main() {
     while (1) {
         scanf("%s", mobileNumber);
 
-        if (mobileNumber[0] == 'q' || mobileNumber[0] == 'Q') {
+        if (mobileNumber[0] == 'q' || mobileNumber[0] == 'Q') {   // q denote end of list of mobile number
             break;
         }
 
-        if (strlen(mobileNumber) != 10) {
+        if (strlen(mobileNumber) != 10||mobileNumber[9]=='0'||mobileNumber[0]=='0'||!isNumeric(mobileNumber)) {
             invalidCount++;
         }
     }
-
     printf("Number of invalid mobile numbers: %d\n", invalidCount);
 
     return 0;
